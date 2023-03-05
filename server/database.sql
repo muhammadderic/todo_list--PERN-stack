a8ef8382b70a5d177dc2d485545486f3f0d86458
muhammadderic <muhammad.donny.e@gmail.com>
2023-03-05 16:43:24 +0700

    create and setting database

---
+++ server/database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE todolist;
+
+CREATE TABLE todos(
+  todo_id SERIAL PRIMARY KEY,
+  description VARCHAR(255) NOT NULL
+);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const express = require("express")
+const cors = require("cors")
+const pool = require("./db")
 
 const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
 
 app.use(() => {
   try {
